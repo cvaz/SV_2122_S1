448c1646c750c735321a8b9899b4c890d155fc27
cvaz <cvaz@cc.isel.ipl.pt>
2021-10-26 22:38:06 +0100

    Initial commit

---
+++ src/main/kotlin/serie1/Arrays.kt
@@ -0,0 +1,18 @@
+package serie1
+
+fun findMinDifference(elem1:IntArray, elem2:IntArray):Int{
+    throw UnsupportedOperationException()
+}
+
+fun partitionPoint(v: IntArray, element: Int): Int {
+    throw UnsupportedOperationException()
+}
+
+fun countSubKSequences(a:IntArray, k:Int):Int{
+    throw UnsupportedOperationException()
+}
+fun median(v:IntArray, l:Int, r:Int):Int{
+    throw UnsupportedOperationException()
+}
+
+
